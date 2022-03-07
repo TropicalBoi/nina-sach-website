0c50640727e8ac34cc94bd5517995692323560f5
TropicalBoi <tewprai.bualoi@gmail.com>
2022-03-07 07:19:35 +0700

    add asterisk as home button

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,16 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import { css } from "@emotion/css";
+import { css, keyframes } from "@emotion/css";
+import asterisk from "../images/Asterisk.png";
+
+const rotate = keyframes`
+  0%{
+    transform: rotate(0deg);
+  }
+  100%{
+    transform: rotate(360deg);
+  }
+`;
 
 const Navbar = () => {
   return (
@@ -12,7 +22,7 @@ const Navbar = () => {
         width: 100vw;
         display: flex;
         flex-direction: row;
-        align-items: flex-start;
+        align-items: flex-end;
         justify-content: space-between;
         z-index: 999;
       `}
@@ -29,16 +39,18 @@ const Navbar = () => {
       >
         <p>WORKS</p>
       </Link>
-      <Link
-        className={css`
-          font-family: RuneScape-Quill-Caps;
-          font-size: 50px;
-          position: relative;
-          color: #ffffff;
-        `}
-        to="/"
-      >
-        <p>*</p>
+      <Link to="/">
+        <img
+          src={asterisk}
+          className={css`
+            width: 1.8vw;
+            height: auto;
+            animation: ${rotate} 8s linear infinite;
+            &:hover {
+              animation-play-state: paused;
+            }
+          `}
+        />
       </Link>
       <Link
         className={css`
